45068e469985ec76c5b27c4ee530a422633a8b6a
Charles Muldoon <charm3728@gmail.com>
2020-08-01 00:31:21 -0700

    began frontend

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// import _ from "lodash";
+// function component() {
+//   const element = document.createElement("div");
+
+const { RenderStuff } = require("../Render");
+
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = "Hey"
+
+//   return element;
+// }
+
+// document.querySelector("#main").appendChild(component())
+document.addEventListener("DOMContentLoaded", function () {
+  async function getData(url) {
+    const res = await fetch(url)
+    return res.json()
+  }
+  async function fetchProducts() {
+    const products = await getData("https://protected-fortress-19687.herokuapp.com/api/products")
+    console.log(products)
+    RenderStuff(products.products);
+  }
+  fetchProducts();
+});
